c17583268d6f8a96b519082570f4defb9b40694e
Abhijit Kailas Gadhave <91112733+AbhiGadhave11@users.noreply.github.com>
2022-03-19 23:58:11 +0530

    Create Ans.c

---
+++ QUESTION_37/Ans.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+
+bool Search(int col,int Arr[][col],int row,int no)
+{
+	bool flag = false;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			if(no == Arr[i][j])
+			{
+				flag = true;
+				break;
+			}
+		}
+	}
+	return flag;
+}
+
+int main()
+{
+	
+	int row = 0,col = 0;
+	printf("Enter the Rows and Columns : \n");
+	scanf("%d%d",&row,&col);
+
+	int Arr[row][col];
+
+	printf("Enter the Elements : \n");
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			scanf("%d",&Arr[i][j]);
+		}
+	}
+	int no = 0;
+	printf("Enter the Element that you want to search : \n");
+	scanf("%d",&no);
+
+	bool bRet = Search(row,Arr,col,no);
+
+	if(bRet == true)
+		printf("Element is there\n");
+	else
+		printf("Element is not there\n");
+	return 0;
+}
